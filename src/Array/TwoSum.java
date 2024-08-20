8bf9eb7609b585876f9c5b7ad49cab9d031f5e12
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-08-20 23:39:50 +0530

    Two sum

---
+++ src/Array/TwoSum.java
@@ -0,0 +1,61 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+//Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target value: ");
+        int target=sc.nextInt();
+        System.out.println("Maximum consecutive ones : "+ Arrays.toString(twoSum(arr,n,target)));
+    }
+
+
+
+//  naive approach
+//    private static int[] twoSum(int[] arr, int n, int target) {
+//        int ans[] = new int[2];
+//        for(int i=0 ; i< n ; i++)
+//        {
+//            for(int j=0 ; j<n ; j++)
+//            {
+//
+//                if(i!=j && arr[j]==target-arr[i])
+//                {
+//                    ans[0]=i;
+//                    ans[1]=j;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+
+    // better approach using hashmap
+    private static int[] twoSum(int[] arr, int n, int target) {
+        int[] ans = new int[2];
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i=0 ; i<n ; i++)
+        {
+            if(mpp.containsKey(target-arr[i]))
+            {
+                ans[0]=i;
+                ans[1]=mpp.get(target-arr[i]);
+            }
+
+            mpp.put(arr[i],i);
+        }
+        return ans;
+    }
+
+}
