a01bb182793cdea2c80a55d29704f7e7135756c4
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-08-29 18:30:19 +0530

    Triplet sum equal to zero

--- src/Array/TripletSum.java
+++ src/Array/TripletSum.java
@@ -88,6 +88,8 @@ public class TripletSum {
         for(int i = 0 ; i< n ; i++)
         {
             if(i!=0 && arr[i]==arr[i-1])    continue;
+            if(arr[i]>0)
+                break;
             int j=i+1 ;
             int k=n-1;
             while(j<k)
@@ -100,7 +102,7 @@ public class TripletSum {
                     j++;
                     k--;
                     while(j<k && arr[j]==arr[j-1])  j++;
-                    while(j<k && arr[k]==arr[k+1])  k++;
+                    while(j<k && arr[k]==arr[k+1])  k--;
                 } else if (sum<0) {
                     j++;
                 }
