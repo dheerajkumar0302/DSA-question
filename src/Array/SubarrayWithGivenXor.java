b2f787860b4bd5474901f0a48e99b9c322bcde61
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-08-29 22:06:59 +0530

    count of subarray with given xor

---
+++ src/Array/SubarrayWithGivenXor.java
@@ -0,0 +1,55 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SubarrayWithGivenXor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter xor value: ");
+        int target = sc.nextInt();
+        System.out.println(subarrayWithGivenXor(arr,target));
+    }
+
+
+    // naive method
+//    private static int subarrayWithGivenXor(int[] arr, int target) {
+//        int cnt=0;
+//        for(int i=0;i<arr.length;i++)
+//        {
+//            int xor=0;
+//            for(int j = i ; j<arr.length ; j++)
+//            {
+//                xor^=arr[j];
+//                if(xor==target)
+//                    cnt++;
+//            }
+//        }
+//        return cnt;
+//    }
+
+    // optimal approach
+    private static int subarrayWithGivenXor(int[] arr, int target) {
+        int cnt=0;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int xr=0;
+        mpp.put(xr,1);
+        for(int i=0 ; i<arr.length;i++)
+        {
+            xr^=arr[i];
+            int x = xr^target;
+            if(mpp.containsKey(x))
+                cnt+=mpp.get(x);
+
+            mpp.put(xr,mpp.getOrDefault(xr,0)+1);
+        }
+        return cnt;
+    }
+}
