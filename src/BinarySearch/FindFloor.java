2b785e2d2e10490997390d1706620427c9f7da66
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-09-08 00:52:47 +0530

    ciel and  floor

--- src/BinarySearch/FindFloor.java
+++ src/BinarySearch/FindFloor.java
@@ -37,27 +37,3 @@ public class FindFloor {
         return high;
     }
 }
-
-
-// alternate approach
-
-
-// int low = 0 ;
-//        int high = nums.length-1;
-//        int ans=n;
-//        while(low<=high)
-//        {
-//            int mid =low+(high-low)/2;
-//            if(nums[mid]>=x)
-//                {
-//                    ans= mid;
-//                    high=mid-1;
-//                }
-//            else
-//            {
-//                low=mid+1;
-//            }
-//
-//        }
-//
-//        return ans;
