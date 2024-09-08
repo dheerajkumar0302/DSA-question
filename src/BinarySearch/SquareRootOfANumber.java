bd83c71cb89244613e0cfdf6491a539dea2c541d
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-09-08 15:02:09 +0530

    Square root of a number

---
+++ src/BinarySearch/SquareRootOfANumber.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+import java.util.Scanner;
+
+public class SquareRootOfANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number");
+        long n = sc.nextLong();
+        System.out.println(floorSqrt(n));
+    }
+
+    private static long floorSqrt(long n) {
+        if(n==1)
+            return 1;
+        long low=0;
+        long high=n-1;
+        while(low<=high)
+        {
+            long mid = low+(high-low)/2;
+            long sq=mid*mid;
+            if(sq==n)
+                return mid;
+            else if(sq>n)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+
+        return high;
+    }
+}
