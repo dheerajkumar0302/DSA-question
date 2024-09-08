e2964fa15e1d0026ca79862408e86db2e27f9b6d
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-09-08 19:09:54 +0530

    SearchInSortedArrays2

--- src/BinarySearch/SearchInSortedArrays2.java
+++ src/BinarySearch/SearchInSortedArrays2.java
@@ -1,5 +1,6 @@
 package BinarySearch;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SearchInSortedArrays2 {
@@ -19,6 +20,7 @@ public class SearchInSortedArrays2 {
     }
     private static boolean search(int[] nums , int target)
     {
+        Arrays.sort(nums);
         int low = 0 ;
         int high = nums.length-1;
         while(low<=high)
