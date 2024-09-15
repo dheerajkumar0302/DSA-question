792d40603c282bd0cc21dad2e8ebee5e0a29a29a
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-09-15 15:27:53 +0530

    TripletSum

--- src/Array/TripletSum.java
+++ src/Array/TripletSum.java
@@ -83,34 +83,34 @@ public class TripletSum {
     // Optimal Solution
     // double pointer approach
     private static List<List<Integer>> tripletSum(int[] arr, int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
-        for(int i = 0 ; i< n ; i++)
-        {
-            if(i!=0 && arr[i]==arr[i-1])    continue;
-            if(arr[i]>0)
-                break;
-            int j=i+1 ;
-            int k=n-1;
-            while(j<k)
+            List<List<Integer>> ans = new ArrayList<>();
+            Arrays.sort(arr);
+            for(int i = 0 ; i< n ; i++)
             {
-                int sum=arr[i]+arr[j]+arr[k];
-                if(sum==0)
+                if(i!=0 && arr[i]==arr[i-1])    continue;
+                if(arr[i]>0)
+                    break;
+                int j=i+1 ;
+                int k=n-1;
+                while(j<k)
                 {
-                    List<Integer> temp = Arrays.asList(arr[i],arr[j],arr[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    while(j<k && arr[j]==arr[j-1])  j++;
-                    while(j<k && arr[k]==arr[k+1])  k--;
-                } else if (sum<0) {
-                    j++;
-                }
-                else {
-                    k--;
+                    int sum=arr[i]+arr[j]+arr[k];
+                    if(sum==0)
+                    {
+                        List<Integer> temp = Arrays.asList(arr[i],arr[j],arr[k]);
+                        ans.add(temp);
+                        j++;
+                        k--;
+                        while(j<k && arr[j]==arr[j-1])  j++;
+                        while(j<k && arr[k]==arr[k+1])  k--;
+                    } else if (sum<0) {
+                        j++;
+                    }
+                    else {
+                        k--;
+                    }
                 }
             }
-        }
-        return ans;
+            return ans;
     }
 }
