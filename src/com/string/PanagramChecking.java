a39d62724e2ff765f3d63b1e91a393c3f201c25e
dheerajkumar0302 <dheerajkumar433597@gmail.com>
2024-10-23 17:44:11 +0530

    commit

---
+++ src/com/string/PanagramChecking.java
@@ -0,0 +1,32 @@
+package com.string;
+
+import java.util.Scanner;
+
+public class PanagramChecking {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println(isPanagram(str));
+    }
+
+    private static boolean isPanagram(String str) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                freq[ch - 'a']++;
+            } else if (ch >= 'A' && ch <= 'Z') {  // Corrected here
+                freq[ch - 'A']++;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
